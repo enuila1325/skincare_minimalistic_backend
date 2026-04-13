9eab2dc90be3c3357fa0aef0af971c857b470258
enuila1325 <enuila634@gmail.com>
2026-04-13 16:34:03 -0600

    Handling multiple images for a single product

--- src/models/Product.js
+++ src/models/Product.js
@@ -5,7 +5,7 @@ const productSchema = new mongoose.Schema(
     name: { type: String, required: true },
     description: { type: String, required: false },
     price: { type: Number, required: true },
-    image: { type: String, required: true },
+    images: [{ type: String }],
     featured: { type: Boolean, default: false },
     stock: { type: Number, default: 0 },
     fechaCreacion: { type: Date, default: Date.now },
