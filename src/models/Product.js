8928b03eab82f8869a1535e3e8990de0508d8106
enuila1325 <enuila634@gmail.com>
2026-04-13 14:01:03 -0600

    Small fix on category -> subcategory populate

--- src/models/Product.js
+++ src/models/Product.js
@@ -5,7 +5,6 @@ const productSchema = new mongoose.Schema(
     name: { type: String, required: true },
     description: { type: String, required: false },
     price: { type: Number, required: true },
-    category: { type: String, required: true },
     image: { type: String, required: true },
     featured: { type: Boolean, default: false },
     stock: { type: Number, default: 0 },
